127b0dd04086a2f2798d1d04feec9c65546c60dc
akihirao <akihirao@gmail.com>
2021-05-17 19:03:55 +0900

    update

--- Pipe.02.RemoveMultipleMappedReads.sh
+++ Pipe.02.RemoveMultipleMappedReads.sh
@@ -8,9 +8,9 @@ set -exuo pipefail
 
 SCRIPT_DIR=$(cd $(dirname $0) && pwd)
 
-no_thread=16
+no_thread=64
 
-reference_folder=/home/akihirao/work/Traja/RefGenome/RefGenome_v1
+reference_folder=/home/akihirao/work/Traja/RefGenome/RefGenome_v2
 main_folder=/home/akihirao/work/Traja/Traja_GRASDi
 
 #samtools 1.12-12-g38139f7
@@ -19,8 +19,8 @@ main_folder=/home/akihirao/work/Traja/Traja_GRASDi
 
 
 #Checking for gatk reference index (*.dict)
-if [ ! -e $reference_folder/agi1.dict ]; then
-	gatk CreateSequenceDictionary -R $reference_folder/agi1.fa -O $reference_folder/agi1.dict
+if [ ! -e $reference_folder/agi1.2.dict ]; then
+	gatk CreateSequenceDictionary -R $reference_folder/agi1.2.fa -O $reference_folder/agi1.2.dict
 fi
 
 
@@ -31,11 +31,11 @@ while read sample; do
 
 	#unique alignment: MAPQ = 50; two alignments: MAPQ = 3, more than three alignment: MAPQ = 0
 	#see detailed in http://yuifu.github.io/remove-multi-reads/ (in Japanese)
-	samtools view -@ $no_thread -b -q 4 $sample.agi1.bam > $sample.agi1.filtered.bam
-	samtools index -@ $no_thread $sample.agi1.filtered.bam
+	samtools view -@ $no_thread -b -q 4 $sample.agi1.2.bam > $sample.agi1.2.filtered.bam
+	samtools index -@ $no_thread $sample.agi1.2.filtered.bam
 
-done < $SCRIPT_DIR/sample_ID.test.list  #list of MIDs
-#done < $SCRIPT_DIR/sample_ID.list  #list of MIDs
+#done < $SCRIPT_DIR/sample_ID.test.list  #list of MIDs
+done < $SCRIPT_DIR/sample_ID.list  #list of MIDs
 
 cd $SCRIPT_DIR
 
