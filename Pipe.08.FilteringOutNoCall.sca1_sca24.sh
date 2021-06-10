c50a578c35d8617a55715c8fb17b672d9d66193d
akihirao <akihirao@gmail.com>
2021-06-10 18:02:57 +0900

    update

---
+++ Pipe.08.FilteringOutNoCall.sca1_sca24.sh
@@ -0,0 +1,44 @@
+#!/bin/bash -i
+#Pipe.08.FilteringOutNoCall.sca1_sca24.sh
+#by HIRAO Akira
+
+set -exuo pipefail
+
+SCRIPT_DIR=$(cd $(dirname $0)  && pwd)
+
+no_thread=64
+
+reference_folder=/home/akihirao/work/Traja/RefGenome/RefGenome_v2
+main_folder=/home/akihirao/work/Traja/Traja_GRASDi
+
+target_ID=Traja_GRASDi
+work_folder=$main_folder/vcf_out
+mkdir -p $work_folder
+
+
+cd $work_folder
+
+
+
+#Set filtered sites to no call:SNP
+#set filtering out locus with no genotypes 99%
+gatk SelectVariants\
+ -R $reference_folder/agi1.2.fa\
+ -V $target_ID.sca1_sca24.snp.DPfilterNoCall.vcf.gz\
+ --set-filtered-gt-to-nocall\
+ --max-nocall-fraction 0.99\
+ --exclude-filtered\
+ -O $target_ID.sca1_sca24.snp.DPfilterNoCall.P99.vcf.gz
+
+
+#Set filtered sites to no call:INDEL
+#set filtering out locus with no genotypes 99%
+gatk SelectVariants\
+ -R $reference_folder/agi1.2.fa\
+ -V $target_ID.sca1_sca24.snp.DPfilterNoCall.vcf.gz\
+ --set-filtered-gt-to-nocall\
+ --max-nocall-fraction 0.99\
+ --exclude-filtered\
+ -O $target_ID.sca1_sca24.indel.DPfilterNoCall.P99.vcf.gz
+
+#=====================================================================
