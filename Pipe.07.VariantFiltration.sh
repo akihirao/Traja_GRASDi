5d7f8ccf60a0f43acb337e5500e8e062448d1189
Akira Hirao <65455824+akihirao@users.noreply.github.com>
2021-06-30 12:34:28 +0900

    update

--- Pipe.07.VariantFiltration.sh
+++ Pipe.07.VariantFiltration.sh
@@ -11,13 +11,30 @@ no_thread=64
 reference_folder=/home/akihirao/work/Traja/RefGenome/RefGenome_v2
 main_folder=/home/akihirao/work/Traja/Traja_GRASDi
 
-target_ID=Traja_GRASDi
-output_folder=$main_folder/vcf_out
+target_ID=Traja_GRASDi_2019_2020
+output_folder=$main_folder/vcf_out_2019_2020
 mkdir -p $output_folder
 
+cd $output_folder
 
-cd $work_folder
+#===============================================
+#Filtering out unplaced & mt + deplicated indiv
+gatk SelectVariants\
+ -R $reference_folder/agi1.2.fa\
+ -V $target_ID.sca_all.snp.vcf.gz\
+ -XL unplaced_mt.list\
+ -xl-ids deplicate_ids.list\
+ -O $target_ID.sca1_24.snp.vcf.gz
 
+gatk SelectVariants\
+ -R $reference_folder/agi1.2.fa\
+ -V $target_ID.sca_all.indel.vcf.gz\
+ -XL unplaced_mt.list\
+ -xl-ids deplicate_ids.list\
+ -O $target_ID.sca1_24.indel.vcf.gz
+#===============================================
+ 
+ 
 #defining ExcessHet parameter
 #"ExcessHet > 13.0" means excess of heterozygosity with p value 0.05 
 ExcessHet_P=0.05
@@ -30,7 +47,7 @@ echo $ExcessHet_param
 #Making select sited that having ExcessHet
 gatk SelectVariants\
  -R $reference_folder/agi1.2.fa\
- -V $target_ID.vcf.gz\
+ -V $target_ID.sca_all.vcf.gz\
  -select "${ExcessHet_param}"\
  -O $target_ID.ExcessHet.vcf
 
@@ -42,16 +59,16 @@ perl $SCRIPT_DIR/ClusterBedBlock.pl < $target_ID.ExcessHet.cluster.bed > $target
 #filtering out ExcessHetblock:SNP
 gatk SelectVariants\
  -R $reference_folder/agi1.2.fa\
- -V $target_ID.snp.vcf.gz\
+ -V $target_ID.sca1_24.snp.vcf.gz\
  --exclude-intervals $target_ID.ExcessHet.block.bed\
- -O $target_ID.snp.no_ExcessHetBlock.vcf.gz \
+ -O $target_ID.sca1_24.snp.no_ExcessHetBlock.vcf.gz \
 
 #Set filtered sites to no call:INDEL
 gatk SelectVariants\
  -R $reference_folder/agi1.2.fa\
- -V $target_ID.indel.vcf.gz\
+ -V $target_ID.sca1_24.indel.vcf.gz\
  --exclude-intervals $target_ID.ExcessHet.block.bed\
- -O $target_ID.indel.no_ExcessHetBlock.vcf.gz \
+ -O $target_ID.sca1_24.indel.no_ExcessHetBlock.vcf.gz \
 #=====================================================================
 
 
@@ -59,7 +76,7 @@ gatk SelectVariants\
 #VariantFiltration for SNP
 gatk VariantFiltration\
  -R $reference_folder/agi1.2.fa\
- -V $target_ID.snp.no_ExcessHetBlock.vcf.gz \
+ -V $target_ID.sca1_24.snp.no_ExcessHetBlock.vcf.gz \
  --filter-expression "QD < 2.0" --filter-name "QDlt2"\
  --filter-expression "FS > 60.0" --filter-name "FSgt60"\
  --filter-expression "MQ < 40.0" --filter-name "MQlt40"\
@@ -69,73 +86,73 @@ gatk VariantFiltration\
  --filter-expression "${ExcessHet_param}" --filter-name "ExHet"\
  -O $target_ID.snp.filter.vcf
 
-grep -E '^#|PASS' $target_ID.snp.filter.vcf | bgzip > $target_ID.snp.filterPASSED.vcf.gz
-tabix -f -p vcf $target_ID.snp.filterPASSED.vcf.gz
+grep -E '^#|PASS' $target_ID.sca1_24.snp.filter.vcf | bgzip > $target_ID.sca1_24.snp.filterPASSED.vcf.gz
+tabix -f -p vcf $target_ID.sca1_24.snp.filterPASSED.vcf.gz
 
 
 #VariantFiltration for INDEL
 gatk VariantFiltration\
  -R $reference_folder/agi1.2.fa\
- -V $target_ID.indel.no_ExcessHetBlock.vcf.gz \
+ -V $target_ID.sca1_24.indel.no_ExcessHetBlock.vcf.gz \
  --filter-expression "QD < 2.0" --filter-name "QDlt2"\
  --filter-expression "FS > 200.0" --filter-name "FSgt200"\
  --filter-expression "ReadPosRankSum < -20.0" --filter-name "RPRSltnagative20"\
  --filter-expression "SOR > 10.0" --filter-name "SORgt10"\
  --filter-expression "${ExcessHet_param}" --filter-name "ExHet"\
- -O $target_ID.indel.filter.vcf
+ -O $target_ID.sca1_24.indel.filter.vcf
 
-grep -E '^#|PASS' $target_ID.indel.filter.vcf | bgzip > $target_ID.indel.filterPASSED.vcf.gz
-tabix -f -p vcf $target_ID.indel.filterPASSED.vcf.gz
+grep -E '^#|PASS' $target_ID.sca1_24.indel.filter.vcf | bgzip > $target_ID.sca1_24.indel.filterPASSED.vcf.gz
+tabix -f -p vcf $target_ID.sca1_24.indel.filterPASSED.vcf.gz
 
 
 #---------------------------------------------------------------------------------------------------------------
 #DepthFiltering for SNP: DP < 10 & GQ <20 (Low Genotype Quality: less than 99%)
 gatk VariantFiltration\
  -R $reference_folder/agi1.2.fa\
- -V $target_ID.snp.filterPASSED.vcf.gz\
+ -V $target_ID.sca1_24.snp.filterPASSED.vcf.gz\
  -G-filter "GQ < 20"\
  -G-filter-name "lowGQ"\
  -G-filter "DP < 10"\
  -G-filter-name "DP_10"\
- -O $target_ID.snp.DPfilterPASSED.vcf.gz
+ -O $target_ID.sca1_24.snp.DPfilterPASSED.vcf.gz
 
 #DepthFiltering for INDEL: DP < 10 & GQ < 20 (Low Genotype Quality: less than 99%) 
 gatk VariantFiltration\
  -R $reference_folder/agi1.2.fa\
- -V $target_ID.indel.filterPASSED.vcf.gz\
+ -V $target_ID.sca1_24.indel.filterPASSED.vcf.gz\
  -G-filter "GQ < 20"\
  -G-filter-name "lowGQ"\
  -G-filter "DP < 10"\
  -G-filter-name "DP_10"\
- -O $target_ID.indel.DPfilterPASSED.vcf.gz
+ -O $target_ID.sca1_24.indel.DPfilterPASSED.vcf.gz
 
 #Set filtered sites to no call:SNP
 gatk SelectVariants\
  -R $reference_folder/agi1.2.fa\
- -V $target_ID.snp.DPfilterPASSED.vcf.gz\
+ -V $target_ID.sca1_24.snp.DPfilterPASSED.vcf.gz\
  --set-filtered-gt-to-nocall\
- -O $target_ID.snp.DPfilterNoCall.vcf.gz
-bcftools index -f $target_ID.snp.DPfilterNoCall.vcf.gz
-bcftools view $target_ID.snp.DPfilterNoCall.vcf.gz -Ov -o $target_ID.snp.DPfilterNoCall.vcf
+ -O $target_ID.sca1_24.snp.DPfilterNoCall.vcf.gz
+bcftools index -f $target_ID.sca1_24.snp.DPfilterNoCall.vcf.gz
+bcftools view $target_ID.sca1_24.snp.DPfilterNoCall.vcf.gz -Ov -o $target_ID.sca1_24.snp.DPfilterNoCall.vcf
 
 #Set filtered sites to no call:INDEL
 gatk SelectVariants\
  -R $reference_folder/agi1.2.fa\
- -V $target_ID.indel.DPfilterPASSED.vcf.gz\
+ -V $target_ID.sca1_24.indel.DPfilterPASSED.vcf.gz\
  --set-filtered-gt-to-nocall\
- -O $target_ID.indel.DPfilterNoCall.vcf.gz
-bcftools index -f $target_ID.indel.DPfilterNoCall.vcf.gz
-bcftools view $target_ID.indel.DPfilterNoCall.vcf.gz -Ov -o $target_ID.indel.DPfilterNoCall.vcf
+ -O $target_ID.sca1_24.indel.DPfilterNoCall.vcf.gz
+bcftools index -f $target_ID.sca1_24.indel.DPfilterNoCall.vcf.gz
+bcftools view $target_ID.sca1_24.indel.DPfilterNoCall.vcf.gz -Ov -o $target_ID.sca1_24.indel.DPfilterNoCall.vcf
 #=====================================================================
 
 
 #Merge SNPs and INDELs vcf files into a SNV vcf file
-gatk MergeVcfs\
- -R $reference_folder/agi1.2.fa\
- -I $target_ID.indel.DPfilterNoCall.vcf.gz\
- -O $target_ID.snp.indel.DPfilterNoCall.vcf.gz
-bcftools index -f $target_ID.snp.indel.DPfilterNoCall.vcf.gz
-bcftools view  $target_ID.snp.indel.DPfilterNoCall.vcf.gz -Ov -o  $target_ID.snp.indel.DPfilterNoCall.vcf
+#gatk MergeVcfs\
+# -R $reference_folder/agi1.2.fa\
+# -I $target_ID.indel.DPfilterNoCall.vcf.gz\
+# -O $target_ID.snp.indel.DPfilterNoCall.vcf.gz
+#bcftools index -f $target_ID.snp.indel.DPfilterNoCall.vcf.gz
+#bcftools view  $target_ID.snp.indel.DPfilterNoCall.vcf.gz -Ov -o  $target_ID.snp.indel.DPfilterNoCall.vcf
 
 
 cd $SCRIPT_DIR
