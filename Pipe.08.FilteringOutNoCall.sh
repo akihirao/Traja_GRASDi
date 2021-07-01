4d1e0df30712f7b4120774259e8faefa4dc9adc4
akihirao <akihirao@gmail.com>
2021-07-01 10:25:16 +0900

    update

---
+++ Pipe.08.FilteringOutNoCall.sh
@@ -0,0 +1,58 @@
+#!/bin/bash -i
+#Pipe.08.FilteringOutNoCall.sca1_sca24.sh
+#by HIRAO Akira
+
+set -exuo pipefail
+
+SCRIPT_DIR=$(cd $(dirname $0)  && pwd)
+
+no_thread=128
+
+reference_folder=/home/akihirao/work/Traja/RefGenome/RefGenome_v2
+main_folder=/home/akihirao/work/Traja/Traja_GRASDi
+
+target_ID=Traja_GRASDi_2019_2020
+work_folder=$main_folder/vcf_out_2019_2020
+mkdir -p $work_folder
+
+
+cd $work_folder
+
+#Filtering out samples with label repetation: SNP
+gatk SelectVariants\
+ -R $reference_folder/agi1.2.fa\
+ -V $target_ID.sca1_24.snp.DPfilterNoCall.vcf.gz\
+ --exclude-sample-name $SCRIPT_DIR/Traja_GRASDi.LabelRepetation.IndivRepetation.args\
+ -O $target_ID.sca1_24.snp.DPfilterNoCall.non_rep.vcf.gz
+
+#Filtering out samples with label repetation: INDEL
+gatk SelectVariants\
+ -R $reference_folder/agi1.2.fa\
+ -V $target_ID.sca1_24.indel.DPfilterNoCall.vcf.gz\
+ --exclude-sample-name $SCRIPT_DIR/Traja_GRASDi.LabelRepetation.IndivRepetation.args\
+ -O $target_ID.sca1_24.indel.DPfilterNoCall.non_rep.vcf.gz
+
+
+#Set filtered sites to no call: SNP
+#set filtering out locus with no genotypes 99%
+gatk SelectVariants\
+ -R $reference_folder/agi1.2.fa\
+ -V $target_ID.sca1_24.snp.DPfilterNoCall.non_rep.vcf.gz\
+ --set-filtered-gt-to-nocall\
+ --max-nocall-fraction 0.99\
+ --exclude-filtered\
+ -O $target_ID.sca1_24.snp.DPfilterNoCall.non_rep.P99.vcf.gz
+
+
+#Set filtered sites to no call: INDEL
+#set filtering out locus with no genotypes 99%
+gatk SelectVariants\
+ -R $reference_folder/agi1.2.fa\
+ -V $target_ID.sca1_24.indel.DPfilterNoCall.non_rep.vcf.gz\
+ --set-filtered-gt-to-nocall\
+ --max-nocall-fraction 0.99\
+ --exclude-filtered\
+ -O $target_ID.sca1_24.indel.DPfilterNoCall.non_rep.P99.vcf.gz
+
+cd $SCRIPT_DIR
+
