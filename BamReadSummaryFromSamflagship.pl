512a29146f6eef963da25620d2f7a846a7d0ba6f
akihirao <akihirao@gmail.com>
2021-10-28 10:41:58 +0900

    update

--- BamReadSummaryFromSamflagship.pl
+++ BamReadSummaryFromSamflagship.pl
@@ -2,7 +2,7 @@
 #BamReadSummaryFromSamflagship.pl
 #by HIRAO Akira
 
-print "Sample","\t","Total_reads", "\t","Mapped_reads", "\t","Mapped_reads_rate", "\t","Properly_paired_mapped_reads", "\t","Properly_paired_mapped_reads_rate","\n";
+print "Sample",",","Total_reads", ",","Mapped_reads", ",","Mapped_reads_rate", ",","Properly_paired_mapped_reads", ",","Properly_paired_mapped_reads_rate","\n";
 
 
 while ($line = <>) {
@@ -10,11 +10,11 @@ while ($line = <>) {
 	if($line =~ m/bam/){
 		@sample_ID = split (/\./, $line); 
 		$ID_out = $sample_ID[0];	
-		print $ID_out, "\t";
+		print $ID_out, ",";
 
 	$line = <>;
 		@total_reads = split (/\s/, $line); 
-		print $total_reads[0], "\t";
+		print $total_reads[0], ",";
 	$line = <>;
 	$line = <>;
 	$line = <>;
@@ -24,7 +24,7 @@ while ($line = <>) {
 		$no_mapped_reads = $mapped_reads[0];
 		$mapped_reads_rate = $mapped_reads[4];
 		$mapped_reads_rate = substr($mapped_reads_rate, 1);
-		print $no_mapped_reads, "\t", $mapped_reads_rate, "\t";
+		print $no_mapped_reads, ",", $mapped_reads_rate, ",";
 
 	$line = <>;
 	$line = <>;
@@ -35,7 +35,7 @@ while ($line = <>) {
 		$no_properly_paired_mapped_reads = $properly_paired_mapped_reads[0];
 		$properly_paired_mapped_reads_rate = $properly_paired_mapped_reads[5];
 		$properly_paired_mapped_reads_rate = substr($properly_paired_mapped_reads_rate, 1);
-		print $no_properly_paired_mapped_reads, "\t", $properly_paired_mapped_reads_rate, "\n";
+		print $no_properly_paired_mapped_reads, ",", $properly_paired_mapped_reads_rate, "\n";
 
 
 	}else{	
--- Check.MappedReads.flagstats.sh
+++ Check.MappedReads.flagstats.sh
@@ -9,7 +9,7 @@ SCRIPT_DIR=$(cd $(dirname $0) && pwd)
 
 no_thread=64
 
-reference_folder=/home/akihirao/work/Traja/RefGenome/RefGenome_v2
+reference_folder=/home/akihirao/work/Traja/RefGenome/RefGenome_v4
 main_folder=/home/akihirao/work/Traja/Traja_GRASDi
 
 #samtools 1.12-12-g38139f7
@@ -25,12 +25,18 @@ while read sample; do
 	work_folder=$main_folder/bwa_out/$sample
 	cd $work_folder
 
-	echo $sample.agi1.2.bam >> $SCRIPT_DIR/CheckOutMappedReads.raw.bam.txt
-	samtools flagstats --threads $no_thread $sample.agi1.2.bam >> $SCRIPT_DIR/CheckOutMappedReads.raw.bam.txt
-	echo $sample.agi1.2.filtered.bam >> $SCRIPT_DIR/CheckOutMappedReads.filtered.bam.txt
-	samtools flagstats --threads $no_thread $sample.agi1.2.filtered.bam >> $SCRIPT_DIR/CheckOutMappedReads.filtered.bam.txt
+	echo $sample.agi.2.0.hard.filtered.bam >> $SCRIPT_DIR/CheckOutMappedReads.raw.bam.txt
+	samtools flagstats --threads $no_thread $sample.agi.2.0.hard.filtered.bam >> $SCRIPT_DIR/CheckOutMappedReads.raw.bam.txt
+	echo $sample.agi.2.0.filteredDup.bam >> $SCRIPT_DIR/CheckOutMappedReads.filtered.bam.txt
+	samtools flagstats --threads $no_thread $sample.agi.2.0.filteredDup.bam >> $SCRIPT_DIR/CheckOutMappedReads.filtered.bam.txt
 
-done < $SCRIPT_DIR/sample_ID.2nd.list  #list of MIDs
+#done < $SCRIPT_DIR/sample_ID.A0001_A0646.list	
+done < $SCRIPT_DIR/sample_ID.test.list  #list of MIDs
 
 cd $SCRIPT_DIR
+
+perl BamReadSummaryFromSamflagship.pl < CheckOutMappedReads.raw.bam.txt > CheckOutMappedReads.raw.bam.summary.csv
+
+perl BamReadSummaryFromSamflagship.pl < CheckOutMappedReads.filtered.bam.txt > CheckOutMappedReads.filtered.bam.summary.csv
+
 
