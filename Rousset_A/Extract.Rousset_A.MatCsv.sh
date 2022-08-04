56fa3e1ea5df90344ea351a18b23528450df4cf5
akihirao <akihirao@gmail.com>
2022-08-04 17:11:56 +0900

    add Rousset's A

---
+++ Rousset_A/Extract.Rousset_A.MatCsv.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+#Extract.RoussetA.MatCsv.sh
+#SPAGeDiを使って個体間の遺伝的分化係数 Rousset's A (Rousset 2000)を算出し、csv形式のmatrixファイルを出力する
+
+#事前にspagediで算出しておく
+#spagedi < cmd.Chiopi.txt
+
+
+CURRENT_DIR=$(cd $(dirname $0) && pwd)
+
+work_folder=/home/akihirao/work/Traja/Traja_GRASDi/Rousset_A
+
+cd $work_folder
+
+
+spagedi_input_file="spagedi.input.txt"
+spagedi_output_file="spagedi.output.txt"
+
+output_csv="Rousset_A.DistMat.csv"
+
+nInd=(`awk 'NR==3 {print $1}' ${spagedi_input_file}`)
+
+#echo $nInd
+
+nCutCol=$((${nInd}+2));
+#echo $nCutCol
+
+
+input_file="Chiopi.spagedi.output.txt"
+
+cat $spagedi_output_file | grep -A $nCutCol "^Pairwise Rousset's distance between individuals ('a' in Rousset, 2000)$" | sed -e '1,2d' | sed s/"ALL LOCI"/ID/ | awk -F '\t' 'BEGIN{OFS=","}{$1=""; print $0}' | sed 's/^,//g' > $output_csv
+
+cd $CURRENT_DIR
+
---
+++ Rousset_A/vcf2spagedi.r
@@ -0,0 +1,54 @@
+#vcf2spagedi.R
+#Conversion R script form vcf to input file for SPAGeDi
+#Population and Spatial position are treated as virtual information
+
+library(vcfR)
+library(adegenet)
+
+setwd("/home/akihirao/work/Traja/Traja_GRASDi/Rousset_A")
+
+input_vcf_file <- "../vcf_out/Traja_GRASDi_ref2_rev2.nDNA.snp.maf001.LDpruned.vcf.gz"
+output_file <- "spagedi.input.txt"
+
+
+fish_vcf <- vcfR::read.vcfR(input_vcf_file)
+
+#Convert to genind
+fish_genind <- vcfR::vcfR2genind(fish_vcf)
+no_indiv <-nInd(fish_genind)
+pop(fish_genind) <- rep("all", no_indiv)
+no_pop <- nPop(fish_genind)
+
+fish_df <- adegenet::genind2df(fish_genind)
+
+
+fish_mat <- gsub("11", "22", as.matrix(fish_df))
+fish_mat <- gsub("01", "12", fish_mat)
+fish_mat <- gsub("10", "12", fish_mat)
+fish_mat <- gsub("00", "11", fish_mat)
+fish_mat[is.na(fish_mat)] <- "00"
+fish_df <- as.data.frame(fish_mat)
+
+fish_df_spatial <- cbind(data.frame(pop=fish_df[,1],X=numeric(nInd(fish_genind)),Y=numeric(nInd(fish_genind)),fish_df[,-1]))
+
+
+no_coord <- 2　#spatial coord: X and Y were set 0 as unrelalized information
+no_loc <- nLoc(fish_genind)
+no_digit_loc <- 1 #genotypes:11, 12, 22, 00(=NA)
+no_ploidy <- 2
+
+line1 <- "//Input file of SNP dataset for SPAGeDi"
+line2 <- "// #ind #cat #coord #loci #dig_loc #ploidy"
+line3 <- c(no_indiv, no_pop, no_coord, no_loc, no_digit_loc, no_ploidy)
+line4 <- 0 #no distance class info
+line5 <- c("Ind", colnames(fish_df_spatial))
+lineEnd <- "END"
+
+write(line1, file=output_file)
+write(line2, file=output_file, append =TRUE)
+write(line3, file=output_file, sep="\t", ncolumns = length(line3), append =TRUE)
+write(line4, file=output_file, sep="\t", ncolumns = length(line4), append =TRUE)
+write(line5, file=output_file,sep="\t", ncolumns = length(line5), append =TRUE)
+write.table(fish_df_spatial, file=output_file, sep="\t", col.names=F,row.names=T,quote=F, append =TRUE)
+write(lineEnd, file=output_file, sep="\t", ncolumns = length(line5), append =TRUE)
+
