564b7716bbbb8fc440c33fb41a77c48bfbcddfa1
akihirao <akihirao@gmail.com>
2022-11-01 17:53:53 +0900

    update

--- BamReadSummaryFromSamflagship.pl
+++ BamReadSummaryFromSamflagship.pl
@@ -2,7 +2,7 @@
 #BamReadSummaryFromSamflagship.pl
 #by HIRAO Akira
 
-print "Sample",",","Total_reads", ",","Mapped_reads", ",","Mapped_reads_rate", ",","Properly_paired_mapped_reads", ",","Properly_paired_mapped_reads_rate","\n";
+print "Sample",",","Total_reads", ",","Mapped_reads", ",","Mapped_reads_rate", ",","Primary_mapped_reads",",","Primary_mapped_reads_rate",",","Properly_paired_mapped_reads", ",","Properly_paired_mapped_reads_rate","\n";
 
 
 while ($line = <>) {
@@ -18,18 +18,23 @@ while ($line = <>) {
 	$line = <>;
 	$line = <>;
 	$line = <>;
-
+	$line = <>;
+	$line = <>;
 	$line = <>;
 		@mapped_reads = split (/\s/, $line);
 		$no_mapped_reads = $mapped_reads[0];
 		$mapped_reads_rate = $mapped_reads[4];
 		$mapped_reads_rate = substr($mapped_reads_rate, 1);
 		print $no_mapped_reads, ",", $mapped_reads_rate, ",";
-
+	$line = <>;
+		@primary_mapped_reads = split (/\s/, $line);
+		$no_primary_mapped_reads = $primary_mapped_reads[0];
+		$primary_mapped_reads_rate = $primary_mapped_reads[5];
+		$primary_mapped_reads_rate = substr($primary_mapped_reads_rate, 1);
+		print $no_primary_mapped_reads, ",", $primary_mapped_reads_rate, ",";
 	$line = <>;
 	$line = <>;
 	$line = <>;
-
 	$line = <>;
 		@properly_paired_mapped_reads = split (/\s/, $line); 
 		$no_properly_paired_mapped_reads = $properly_paired_mapped_reads[0];
@@ -43,3 +48,4 @@ while ($line = <>) {
 	}
 
 }
+
--- Check.MappedReads.flagstats.sh
+++ Check.MappedReads.flagstats.sh
@@ -7,11 +7,13 @@ set -exuo pipefail
 
 CURRENT_DIR=$(cd $(dirname $0) && pwd)
 
-no_threads=64
+no_threads=48
 
 reference_folder=/home/akihirao/work/Traja/RefGenome/RefGenome_v4
 main_folder=/home/akihirao/work/Traja/Traja_GRASDi
 script_folder=$main_folder/Scripts
+
+code_ID="agi.2.0.rev2"
 
 #samtools 1.12-12-g38139f7
 #Using htslib 1.12-10-gc3ba302
@@ -26,13 +28,13 @@ while read sample; do
 	work_folder=$main_folder/bwa_out/$sample
 	cd $work_folder
 
-	echo $sample.agi.2.0.rev2.bam >> $script_folder/CheckOutMappedReads.raw.bam.txt
-	samtools flagstats --threads $no_threads $sample.agi.2.0.rev2.bam >> $script_folder/CheckOutMappedReads.raw.bam.txt
-	echo $sample.agi.2.0.rev2.filteredDup.bam >> $script_folder/CheckOutMappedReads.filtered.bam.txt
-	samtools flagstats --threads $no_threads $sample.agi.2.0.rev2.filteredDup.bam >> $script_folder/CheckOutMappedReads.filtered.bam.txt
+	echo $sample.$code_ID.bam >> $script_folder/CheckOutMappedReads.raw.bam.txt
+	samtools flagstats --threads $no_threads $sample.$code_ID.rev2.bam >> $script_folder/CheckOutMappedReads.raw.bam.txt
+	echo $sample.$code_ID.filteredDup.bam >> $script_folder/CheckOutMappedReads.filtered.bam.txt
+	samtools flagstats --threads $no_threads $sample.$code_ID.rev2.filteredDup.bam >> $script_folder/CheckOutMappedReads.filtered.bam.txt
 
 done < $script_folder/sample_ID.A0001_A0646.list	
-#done < $script_folder/sample_ID.test.list  #list of MIDs
+
 
 cd $script_folder
 
